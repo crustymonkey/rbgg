51d6983b24ca4f670598eb3cd6cd886bc8d571c3
Jay Deiman <jay@splitstreams.com>
2023-12-04 16:04:26 -0800

    Added another unit test

--- tests/integration-tests.rs
+++ tests/integration-tests.rs
@@ -33,3 +33,39 @@ fn test_search() {
     println!("{}", to_string_pretty(&data).unwrap());
     assert_eq!("Bruges", data["items"]["item"]["name"]["@value"]);
 }
+
+#[test]
+fn test_boardgame() {
+    // API v1
+    let cl = Client1::new_from_defaults();
+    let params = Params::from([
+        ("stats".into(), "1".into()),
+    ]);
+    let res = cl.boardgame_b(&vec![136888, 133473], Some(params));
+    
+    assert!(res.is_ok());
+    let data = res.unwrap();
+    for game in data["boardgames"]["boardgame"].as_array().unwrap() {
+        if game["@objectid"] != "136888" && game["@objectid"] != "133473" {
+            panic!();
+        }
+    }
+    println!("{}", to_string_pretty(&data["boardgames"]["boardgame"]).unwrap());
+
+    // API v2
+    let cl = Client2::new_from_defaults();
+    let params = Params::from([
+        ("stats".into(), "1".into()),
+    ]);
+    let res = cl.boardgame_b(&vec![136888, 133473], Some(params));
+    
+    assert!(res.is_ok());
+    let data = res.unwrap();
+    println!("{}", to_string_pretty(&data).unwrap());
+    for game in data["items"]["item"].as_array().unwrap() {
+        if game["@id"] != "136888" && game["@id"] != "133473" {
+            panic!();
+        }
+    }
+    println!("{}", to_string_pretty(&data["items"]["item"]).unwrap());
+}
